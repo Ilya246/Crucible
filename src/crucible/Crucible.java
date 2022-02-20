938fbd7cdaf80c4c817b881f633cea3b57282d8c
Ilya246 <ilyukarno@gmail.com>
2022-02-20 14:19:41 +0400

    initial setup

--- src/crucible/Crucible.java
+++ src/crucible/Crucible.java
@@ -1,4 +1,4 @@
-package example;
+package crucible;
 
 import arc.*;
 import arc.util.*;
@@ -9,28 +9,19 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class Crucible extends Mod{
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
+    public Crucible(){
+        Log.info("Crucible loaded.");
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("crucible-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
-    }
-
-    @Override
-    public void loadContent(){
-        Log.info("Loading some example content.");
     }
 
 }
